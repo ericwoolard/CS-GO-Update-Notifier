c506fb398edb7b68986e90b8cf390485a6aecd61
Eric Woolard <premierproductions601@gmail.com>
2020-07-28 17:43:11 -0500

    Updated to use slackclient library

--- slack_alerts.py
+++ slack_alerts.py
@@ -1,11 +1,19 @@
-import os
+import requests
+import traceback
+from datetime import datetime
+from slackclient import SlackClient
 
 
-def sendAlert(id):
-    message = '<!channel> *O SHIT WADDUPP!!* UPDATE HYPPEEEE! *New Build ID:* {}'.format(str(id))
-    cmd = 'curl https://slack.com/api/chat.postMessage -X POST -d "channel=#general" -d "text=' \
-          + message + '" -d "username=USERNAME" -d ' \
-                      '"token=TOKEN" -d ' \
-                      '"icon_emoji=OPTIONAL_ICON"'
-    os.system(cmd)
+war_bot = SlackClient('TOKEN')
+channel_name = '#bots'
 
+
+def sendAlert(currentBuild):
+    try:
+        message = '<!channel> *CS:GO UPDATE* \n*O SHIT WADDUPP!!* :cope: UPDATE HYPPEEEE! :pray::gaben::pray: *New Build ID:* {} \n\nhttp://blog.counter-strike.net/index.php/category/updates/'.format(str(currentBuild))
+        res = war_bot.api_call("chat.postMessage", channel=channel_name, text=message, as_user=True)
+
+    except:
+        req_error = traceback.format_exc()
+        now = str(datetime.now())
+        print(now + ' - Error:\n' + req_error + '\n\n\n')
